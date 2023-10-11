a8e6a491b0121c6e880d919d37bf2495f814e5b6
riya <114512743+ria-goyal@users.noreply.github.com>
2023-10-11 11:12:35 +0530

    Delete demo1 directory

--- demo1/script.js
+++
@@ -1,7 +0,0 @@
-let img=document.getElementById('image');
-let button=document.getElementById('btn');
-window.addEventListener('scroll',function(){
-        let value=window.scrollY;
-        img.style.top= value*0.5+'px';
-        
-    });
